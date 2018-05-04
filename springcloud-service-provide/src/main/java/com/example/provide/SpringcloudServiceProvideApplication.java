1516ca967b1e36e1558302cf80fa028caa1e5d61
CPSDC <CPSDC@DESKTOP-7T1PPNB>
2018-05-04 16:37:42 +0800

    d

---
+++ springcloud-service-provide/src/main/java/com/example/provide/SpringcloudServiceProvideApplication.java
@@ -0,0 +1,14 @@
+package com.example.provide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringcloudServiceProvideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudServiceProvideApplication.class, args);
+	}
+}
